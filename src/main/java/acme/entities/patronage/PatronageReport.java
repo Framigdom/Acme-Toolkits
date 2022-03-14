68ebc0987be3d929f471dae9172615349e0c97dd
davzamfer <davzamfer@alum.us.es>
2022-03-14 17:25:23 +0100

    Task 008: Done

---
+++ src/main/java/acme/entities/patronage/PatronageReport.java
@@ -0,0 +1,64 @@
+/*
+ * Chirp.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities.patronage;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PatronageReport extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long		serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	
+	@NotBlank
+	@Pattern(regexp = "<patronage-code>:<serial-number>")
+	protected String sequenceNumber;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date					creationMoment;
+	
+	@NotBlank
+	@Length(max=255)
+	protected String				memorandum;
+	
+	@URL
+	protected String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
