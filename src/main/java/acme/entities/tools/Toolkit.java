72d4870ab85e044054d21e418503855ac54297dd
PabloAlvarezCaroUS <100071510+PabloAlvarezCaroUS@users.noreply.github.com>
2022-03-14 17:23:07 +0100

    Task 006: Created Toolkit class

--- src/main/java/acme/entities/tools/Toolkit.java
+++ src/main/java/acme/entities/tools/Toolkit.java
@@ -4,8 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.PositiveOrZero;
-
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
@@ -16,35 +14,35 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Tool extends AbstractEntity{
+public class Toolkit extends AbstractEntity{
 
 	// Serialisation identifier -----------------------------------------------
 
 	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
-	
-	@NotBlank
-	@Length(max = 100)
-	protected String			name;
-	
+		
 	@Column(unique = true)
 	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	protected String			code;
 	
 	@NotBlank
 	@Length(max = 100)
-	protected String			technology;
+	protected String			title;
 	
 	@NotBlank
 	@Length(max = 255)
 	protected String			description;
 	
-	@PositiveOrZero
-	protected Integer			retailPrice;
+	@NotBlank
+	@Length(max = 255)
+	protected String			assemblyNotes;
 	
 	@URL
 	protected String			link;
-
+	
+	
+	
+	
 	
 }
