efd55af22ad0ed2b545937682c45dc6ed81e8d44
framigdom <framigdom@alum.us.es>
2022-03-15 11:47:23 +0100

    Task 001: Done

---
+++ src/main/java/acme/entities/roles/Patron.java
@@ -0,0 +1,35 @@
+package acme.entities.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Patron extends AbstractEntity {
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String			company;
+	
+	@NotBlank
+	@Length(max = 255)
+	protected String			statement;
+	
+	@URL
+	protected String			link;
+
+}
