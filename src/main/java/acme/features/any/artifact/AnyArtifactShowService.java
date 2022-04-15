9596c5a0377e4ff6761c33ed58f29b2ce78ff577
framigdom <framigdom@alum.us.es>
2022-04-15 13:46:00 +0200

    Task 019:done

--- src/main/java/acme/features/any/artifact/AnyArtifactShowService.java
+++ src/main/java/acme/features/any/artifact/AnyArtifactShowService.java
@@ -33,8 +33,8 @@ public class AnyArtifactShowService implements AbstractShowService<Any, Artifact
 		int id;
 		id=request.getModel().getInteger("id");
 		
-		result = this.repository.findOneAnyComponentById(id);
-
+		result = this.repository.findOneAnyArtifactById(id);
+		
 		return result;
 	}
 	
