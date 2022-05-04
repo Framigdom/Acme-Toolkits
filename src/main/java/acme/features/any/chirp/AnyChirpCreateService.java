70850b95ca5676485afe23d1303553213019ea85
PabloAlvarezCaroUS <100071510+PabloAlvarezCaroUS@users.noreply.github.com>
2022-05-04 16:34:09 +0200

    Task 040: Done

---
+++ src/main/java/acme/features/any/chirp/AnyChirpCreateService.java
@@ -0,0 +1,82 @@
+package acme.features.any.chirp;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.chirps.Chirp;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Errors;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Any;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AnyChirpCreateService implements AbstractCreateService<Any, Chirp> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AnyChirpRepository repository;
+
+	// AbstractCreateService<Administrator, Shout> interface --------------
+
+
+	@Override
+	public boolean authorise(final Request<Chirp> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public Chirp instantiate(final Request<Chirp> request) {
+		assert request != null;
+
+		Chirp result;
+
+		result = new Chirp();
+
+		return result;
+	}
+
+	@Override
+	public void bind(final Request<Chirp> request, final Chirp entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+		request.bind(entity, errors, "title", "author", "body", "email");
+		entity.setMoment(moment);
+	}
+
+	@Override
+	public void validate(final Request<Chirp> request, final Chirp entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+	}
+
+	@Override
+	public void unbind(final Request<Chirp> request, final Chirp entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "title", "author", "body", "email");
+	}
+
+	@Override
+	public void create(final Request<Chirp> request, final Chirp entity) {
+		assert request != null;
+		assert entity != null;
+
+		this.repository.save(entity);
+	}
+
+}
